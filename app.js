eadcfd5ebd7d5740acc35d95c3156d65aa8726ba
Michael Tomcal <mtomcal@cbtnuggets.com>
2016-03-10 14:38:05 -0800

    Broke out code into directories. Working existing code detection for bootstrap generate

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var Rx = require('rxjs');
-var fs = require('fs-extra');
-var devServer = require('./devServer');
+var lib = require('./lib');
+
 var argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
     .command('react-server')
@@ -13,98 +13,18 @@ var argv = require('yargs')
 
 var argStream = Rx.Observable.fromArray(argv._);
 
-argStream
+var serverStream = argStream
     .filter(function (arg) {
         return arg === 'react-server';
     })
-    .take(1)
-    .subscribe(function (arg) {
-        console.log('Starting react-server...');
-        console.log('Using directory: ' + process.cwd());
-        devServer();
-    });
+    .take(1);
 
-
-function copyStream(source, target) {
-    return Rx.Observable.create(function (observable) {
-        fs.copy(source, target, function (err) {
-            if (err) {
-                return observable.error(err);
-            }
-            observable.next();
-            observable.complete();
-        })
-    });
-}
-
-function createDir(target) {
-    return Rx.Observable.create(function (observable) {
-        fs.mkdir(target, function (err) {
-            if (err) {
-                return observable.error(err);
-            }
-            observable.next();
-            observable.complete();
-        })
-    });
-}
-
-var fileManifest = [
-    '.eslintrc',
-    '.babelrc',
-    '.gitignore',
-    'index.html'
-];
-
-var demoFiles = [
-    'App.js',
-    'colors.js',
-    'index.js'
-];
-
-var createDirs = [
-    'src',
-    'dist'
-];
+lib.reactServer(serverStream);
 
 var bootstrapStream = argStream
     .filter(function (arg) {
         return arg === 'bootstrap';
     })
+    .take(1);
 
-bootstrapStream
-    .flatMap(function () {
-        return createDirs;
-    })
-    .do(console.log.bind(console, 'Creating '))
-    .flatMap(function (dir) {
-        return createDir(process.cwd() + '/' + dir)
-            .catch(function (err) {
-                console.warn(dir + ' already exists.');
-                return Rx.Observable.of(true);
-            });
-    })
-    .reduce(function (acc, x) {
-        return x;
-    })
-    .flatMap(function () {
-        return fileManifest;
-    })
-    .do(console.log.bind(console, 'Creating '))
-    .flatMap(function (file) {
-        return copyStream(__dirname + '/bootstrap/' + file, process.cwd() + '/' + file)
-    })
-    .reduce(function (acc, x) {
-        return x;
-    })
-    .flatMap(function () {
-        return demoFiles;
-    })
-    .do(console.log.bind(console, 'Creating '))
-    .flatMap(function (file) {
-        return copyStream(__dirname + '/demo/' + file, process.cwd() + '/src/' + file)
-    })
-    .reduce(function (acc, x) {
-        return x;
-    })
-    .subscribe(null, function (err) { console.error(err.stack)})
+lib.bootstrap(bootstrapStream);
